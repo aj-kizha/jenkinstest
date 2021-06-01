93a23b6bbf2797380f8e0899eab44e36255d4f93
aj-kizha <ajay_kizha@outlook.com>
2021-06-01 21:14:00 +0530

    changed pep standards

--- fetchinsertdynamodb.py
+++ fetchinsertdynamodb.py
@@ -1,5 +1,5 @@
 import requests
 url = 'http://127.0.0.1:9000/api/qualitygates/project_status'
-out = requests.get(url, params={'projectKey':'pythonproj'}, auth=('admin','admin1'))
-#print(("project status is").center(50,'*'))
+out = requests.get(url, params={'projectKey': 'pythonproj'},
+                   auth=('admin', 'admin1'))
 print(out.json())
