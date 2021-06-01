c27c9432a176b58de3b22abb7dd25844a92bd3b9
aj-kizha <ajay_kizha@outlook.com>
2021-06-01 21:53:35 +0530

    added insertcomments

--- fetchinsertdynamodb.py
+++ fetchinsertdynamodb.py
@@ -1,5 +1,15 @@
 import requests
+import json
+import boto3
+
 url = 'http://127.0.0.1:9000/api/qualitygates/project_status'
 out = requests.get(url, params={'projectKey': 'pythonproj'},
                    auth=('admin', 'admin1'))
-print(out.json())
+
+records = {}
+records['projectid'] = 'pythonproj'
+records['condition'] = out.json()['projectStatus']['conditions']
+
+dynamodb = boto3.resource('dynamodb', region_name="ap-south-1")
+table = dynamodb.Table('projectquality')
+table.put_item(Item=records)
