070c495c7be2611adc37acb8c2a637d09d62cc6e
ajay kizhakumkara <40577162+aj-kizha@users.noreply.github.com>
2021-06-08 15:13:37 +0530

    Update fetchinsertdynamodb.py

--- fetchinsertdynamodb.py
+++ fetchinsertdynamodb.py
@@ -22,3 +22,22 @@ dynamodb = boto3.resource('dynamodb', aws_access_key_id=sys.argv[1],
                           region_name="ap-south-1")
 table = dynamodb.Table('projectquality')
 table.put_item(Item=records)
+
+'''Security insertion'''
+securityurl = 'http://127.0.0.1:9000/api/hotspots/search'
+out = requests.get(securityurl,params={'projectKey':'pythonproj'},auth=('admin','admin1'))
+security_out = out.json()
+
+hotspot_record = {}
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+for item in security_out['hotspots']:
+    hotspot_record['projectid'] = item['project']
+    hotspot_record['datetime'] = dt_string
+    hotspot_record['securitycat'] = item['securityCategory']
+    hotspot_record['vulnerabilityProbability'] = item['vulnerabilityProbability']
+    hotspot_record['component'] = item['component']
+    
+security_table = dynamodb.Table('projecthotspot')
+security_table.put_item(Item=records)
