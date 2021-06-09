d2eb8cd679aee517d9ac383064883b4bb1de41d5
ajay kizhakumkara <40577162+aj-kizha@users.noreply.github.com>
2021-06-09 16:28:20 +0530

    Update fetchinsertdynamodb.py

--- fetchinsertdynamodb.py
+++ fetchinsertdynamodb.py
@@ -48,7 +48,7 @@ security_table.put_item(Item=hotspot_record)
 with open('report.json') as fp:
   banditjson = json.load(fp)
 bandit_report = {}
-bandt_report['projectid'] = 'pythonproj'
+bandit_report['projectid'] = 'pythonproj'
 bandit_report['result'] = banditjson
 bandit_table = dynamodb.Table('bandit')
 bandit_table.put_item(Item=bandit_report)
